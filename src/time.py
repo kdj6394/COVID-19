0c7db36641db7f0f34d94eb9128a0e95895599c0
kdj6394 <55368269+kdj6394@users.noreply.github.com>
2020-04-24 23:01:58 +0900

    Update time.py

--- src/time.py
+++ src/time.py
@@ -13,9 +13,8 @@ def skew_normal(x,m,a,s,n):
 
 
 if __name__ == '__main__':
-    # root = sys.argv[1]
-    root = r'C:\Users\82104\Documents\GitHub\COVID-19\data'
-
+    root = sys.argv[1]
+    
     data = pd.read_csv(join(root,"Time.csv"),index_col=0)
     data.index = pd.to_datetime(data.index)
     
